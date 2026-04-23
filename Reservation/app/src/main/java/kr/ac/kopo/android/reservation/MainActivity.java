39da49f0fc11f75531e094a649cb4c77c5a8fe5e
hazyala <heamin0603@naver.com>
2026-04-23 15:29:54 +0900

    Reservation

--- Reservation/app/src/main/java/kr/ac/kopo/android/reservation/MainActivity.java
+++ Reservation/app/src/main/java/kr/ac/kopo/android/reservation/MainActivity.java
@@ -1,14 +1,30 @@
 package kr.ac.kopo.android.reservation;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Chronometer chronometer;
+    RadioGroup rg;
+    CalendarView calendar;
+    TimePicker timePicker;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        chronometer = findViewById(R.id.chronometer);
+        rg = findViewById(R.id.rg);
+        calendar = findViewById(R.id.calender);
+        timePicker = findViewById(R.id.time_picker);
+        textResult = findViewById(R.id.text_result);
+
+        calendar.setVisibility(View.INVISIBLE);
+
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnDone = findViewById(R.id.btn_done);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+                chronometer.setTextColor(Color.RED);
+            }
+        });
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+                chronometer.setTextColor(Color.BLUE);
+            }
+        });
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(@NonNull RadioGroup group, int checkedId) {
+                calendar.setVisibility(View.INVISIBLE);
+                timePicker.setVisibility(View.INVISIBLE);
+                if(checkedId == R.id.radio_date)
+                    calendar.setVisibility(View.VISIBLE);
+                else timePicker.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
