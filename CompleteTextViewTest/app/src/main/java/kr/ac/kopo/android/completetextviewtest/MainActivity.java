1fdceb63ef14b61c8833e129e78baf92a7fab6ac
hazyala <heamin0603@naver.com>
2026-04-30 16:34:38 +0900

    고급위젯-자동완성텍스트뷰, 진행바, 뷰컨테이너(ScrollView)

---
+++ CompleteTextViewTest/app/src/main/java/kr/ac/kopo/android/completetextviewtest/MainActivity.java
@@ -0,0 +1,42 @@
+package kr.ac.kopo.android.completetextviewtest;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView autoComplete;
+    MultiAutoCompleteTextView multiComplete;
+    String[] items = {"ball", "banana", "base", "balloon", "Canada", "carrot", "car", "card", "door"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        autoComplete = findViewById(R.id.auto_complete);
+        multiComplete = findViewById(R.id.multi_complete);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
+
+        autoComplete.setAdapter(adapter);
+
+        MultiAutoCompleteTextView.CommaTokenizer tokenizer = new MultiAutoCompleteTextView.CommaTokenizer();
+        multiComplete.setTokenizer(tokenizer);
+        multiComplete.setAdapter(adapter);
+
+    }
+}
