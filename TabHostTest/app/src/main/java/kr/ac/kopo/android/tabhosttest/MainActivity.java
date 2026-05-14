665b16a511a47dd82fb6b988cfa578e0b6749cce
hazyala <heamin0603@naver.com>
2026-05-14 15:02:03 +0900

    TabHostTest

--- TabHostTest/app/src/main/java/kr/ac/kopo/android/tabhosttest/MainActivity.java
+++ TabHostTest/app/src/main/java/kr/ac/kopo/android/tabhosttest/MainActivity.java
@@ -1,24 +1,36 @@
 package kr.ac.kopo.android.tabhosttest;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // TabHost 객체 생성 및 초기화
+        TabHost tabHost = getTabHost();
+
+        // 강아지 탭 설정
+        TabHost.TabSpec specDog = tabHost.newTabSpec("dog").setIndicator("강아지");
+        specDog.setContent(R.id.tabDog);
+        tabHost.addTab(specDog);
+
+        // 고양이 탭 설정
+        TabHost.TabSpec specCat = tabHost.newTabSpec("cat").setIndicator("고양이");
+        specCat.setContent(R.id.tabCat);
+        tabHost.addTab(specCat);
+
+        // 토끼 탭 설정
+        TabHost.TabSpec specRabbit = tabHost.newTabSpec("rabbit").setIndicator("토끼");
+        specRabbit.setContent(R.id.tabRabbit);
+        tabHost.addTab(specRabbit);
+
+        // 첫 번째 탭을 기본으로 설정
+        tabHost.setCurrentTab(0);
     }
 }
